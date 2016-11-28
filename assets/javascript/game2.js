85eda4a69ce54d0d18e30034d6ac1968604eb4b5
Kristin Esterley <Kristin@Kristins-MBP.attlocal.net>
2016-11-28 16:36:46 -0500

    audio addition

--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,10 +1,18 @@
   
     // Vars and Functions
-    function displayWinnerInfo(countryName, flagImage, numWins) {
+
+
+    function displayWinInfo(countryName, flagImage, numWins, soundString) {
 
               document.querySelector("#tag").innerHTML=countryName;
               document.querySelector("#map").innerHTML=flagImage;
               document.querySelector("#wins").innerHTML = numWins; 
+
+              document.querySelector("#myAudio").innerHTML=soundString;
+              var plyr = ""; 
+              plyr=document.getElementById("myAudio");
+              plyr.load();
+              plyr.play();
     }
     function updateRoundDisplay(userPrompt, guessesLeft, lettersDone, wordPart ){
                 document.querySelector("#message").innerHTML = userPrompt;     
@@ -14,12 +22,34 @@
     }
 
     var game = {
-            words:["MEXICO", "GERMANY", "ARGENTINA", "GUATEMALA", "DJIBOUTI", "UKRAINE"],
-            numGuesses:[10,10,12,12,15,11],
-            wordImage:["assets/images/mexico.jpeg","assets/images/germany.jpeg","assets/images/argentina.jpeg","assets/images/guatemala.jpeg","assets/images/djibouti.jpeg" ,"assets/images/ukraine.jpeg"],
+            words:[ "MEXICO", 
+                    "GERMANY", 
+                    "ARGENTINA", 
+                    "GUATEMALA", 
+                    "DJIBOUTI", 
+                    "UKRAINE"],
+            numGuesses:[10,
+                        10,
+                        12,
+                        12,
+                        15,
+                        11],
+            wordImage:["assets/images/mexico.jpeg",
+                      "assets/images/germany.jpeg",
+                      "assets/images/argentina.jpeg",
+                      "assets/images/guatemala.jpeg",
+                      "assets/images/djibouti.jpeg" ,
+                      "assets/images/ukraine.jpeg"],
+            wordAnthem:["http://www.lengua.com/anthems/Mexico.mp3",
+                        "assets/sound/germany.mp3",
+                        "http://www.lengua.com/anthems/Argentina.mp3",
+                        "http://www.lengua.com/anthems/Guatemala.mp3",
+                        "http://www.lengua.com/anthems/Djibouti.mp3",
+                        "http://www.lengua.com/anthems/Ukraine.mp3"],
             yourWord:"",
             yourGuesses:0,
             yourImage:"",
+            yourAnthem:"",
             lettersGuessed:[],
             wins:0,
             wordInProgress:"",
@@ -35,21 +65,23 @@
                 this.yourWord = "";
                 this.yourGuesses=0;
                 this.yourImage="";
+                this.yourAnthem="";
                 this.wordInProgress = "";
                 this.correctCounter = 0;
                 
               // randomly select a word from the word array
-              // yourWord = words[Math.floor(Math.random() * words.length)];
                 this.yourWord = this.words[Math.floor(Math.random() * this.words.length)];
 
-                //put your word, its number of guesses and its image path into temp variables
+                //put your word, its number of guesses and its image path and the anthem source string into temp variables
                 var yourWordIndex = this.words.indexOf(this.yourWord);
                 this.yourGuesses=this.numGuesses[yourWordIndex];
                 this.yourImage=this.wordImage[yourWordIndex];
-              // once word is chosen, remove word from words array, num guesses from guesses array and image path from wordImage array
+                this.yourAnthem=this.wordAnthem[yourWordIndex];
+              // once word is chosen, remove word from words array, num guesses from guesses array and image path and the anthem source from wordImage array
                 this.words.splice(yourWordIndex, 1);
                 this.numGuesses.splice(yourWordIndex,1);
-                this.wordImage.splice(yourWordIndex,1);           
+                this.wordImage.splice(yourWordIndex,1); 
+                this.wordAnthem.splice(yourWordIndex,1);          
 
               //format the display for the unguessed word      
                 for (var i=0; i < this.yourWord.length; i++){
@@ -67,12 +99,7 @@
 
     }; //end of the object definition
 
-
-
-
       //begin code execution
-
-
         document.querySelector("#wins").innerHTML = game.wins; 
         game.beginRound();
     
@@ -111,10 +138,12 @@
             // if user has guessed all of the letters in the word, then increment the wins, display the winning word and flag image and begin another round, else if user has run out of guesses, increment losses and begin another round. if neither of those, code will then process the next key stroke which is the next letter guess
             if (game.correctCounter === game.yourWord.length) {
               game.wins = game.wins + 1;
+ 
               var imgString = "<img src=\"" + game.yourImage + "\">"
-              displayWinnerInfo(game.yourWord,imgString,game.wins);
-              // document.getElementbyId("myAudio").play();
-              // ="<source src=\" + germany.ogg + \" + type=\"audio/ogg\">";
+              var anthemString = "<source src=\"" + game.yourAnthem + "\" type=\"audio/mp3\">";
+ 
+              displayWinInfo(game.yourWord,imgString,game.wins,anthemString);
+              
               game.beginRound();
             }          
             else if (game.yourGuesses === 0){
